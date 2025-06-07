6ecba4f73d67b665fb3c2e253ba8788e5d543e3a
Krzysztof <66274761+malezjaa@users.noreply.github.com>
2025-06-07 18:40:13 +0200

    feat: add an example of how the format could look like and use glob to collect the files

--- cli/src/commands/build.rs
+++ cli/src/commands/build.rs
@@ -1,8 +1,17 @@
-use marston_core::MResult;
-use marston_core::context::Context;
+use glob::glob;
+use marston_core::{MPath, MResult, context::Context, fs::to_mpath};
 
 pub fn build_command(ctx: Context) -> MResult<()> {
-    // let files =
+    let pattern = format!("{}/**/*.mr", ctx.main_dir());
+
+    let files: Vec<MPath> = glob(&pattern)?
+        .filter_map(Result::ok)
+        .filter_map(|path_buf| to_mpath(path_buf).ok())
+        .collect::<Vec<_>>();
+
+    for path in &files {
+        println!("Found MR file: {}", path);
+    }
 
     Ok(())
 }
--- marston/src/context.rs
+++ marston/src/context.rs
@@ -5,14 +5,23 @@ use std::path::{Path, PathBuf};
 #[derive(Debug)]
 pub struct Context {
     config: Config,
+    cwd: MPath,
 }
 
 impl Context {
     pub fn new(cwd: &MPath) -> MResult<Self> {
-        Ok(Context { config: Config::find_recursively(cwd)? })
+        Ok(Context { config: Config::find_recursively(cwd)?, cwd: cwd.clone() })
     }
 
     pub fn name(&self) -> String {
         self.config.project.name.clone()
+    }
+    
+    pub fn build_dir(&self) -> &MPath {
+        &self.config.build.output_dir
+    }
+    
+    pub fn main_dir(&self) -> &MPath {
+        &self.config.build.main_dir
     }
 }
